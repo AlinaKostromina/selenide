d9c4e9f83bc7c9f6c8c4abdc1992012b3405d8f2
Kostromina Alina <a.kstrmna95@gmail.com>
2023-12-03 21:31:04 +0700

    Test added

---
+++ src/test/java/CardDeliveryFormTest.java
@@ -0,0 +1,34 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import static com.codeborne.selenide.Selenide.*;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class CardDeliveryFormTest {
+
+    @Test
+    void shouldSendForm() {
+        Configuration.headless = true;
+        Configuration.holdBrowserOpen = true;
+
+        String date = LocalDate.now().plusDays(5).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Новосибирск");
+        $("[data-test-id='date'] input").doubleClick();
+        $("[data-test-id='date'] input").sendKeys(Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(date);
+        $("[data-test-id='name'] input").setValue("Костромина Алина");
+        $("[data-test-id='phone'] input").setValue("+79139116137");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(Condition.exactText("Забронировать")).click();
+        $("[data-test-id='notification']").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(Condition.ownText(date));
+    }
+}
+
+
